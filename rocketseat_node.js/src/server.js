d331431761c7110218c5479589393124f8f975e3
Jreng7 <jr.waydev@gmail.com>
2024-12-05 18:42:31 -0300

    criando database.js

--- rocketseat_node.js/src/server.js
+++ rocketseat_node.js/src/server.js
@@ -1,10 +1,13 @@
 import http from 'node:http'
-import { json } from './middlewares/json'
+import { json } from './middlewares/json.js'
+import { Database } from './middlewares/database.js'
 
 /**
  *  CABEÇALHOS (TANTO DA REQUISIÇÃO QUANTO DA RESPOSTA) => SÃO METADADOS!
  *  OS METADADOS SÃO INFORMAÇÕES PARA QUE CADA UM POSSA SABER LIDAR COM REQUISIÇÃO OU RESPOSTA.
  */
+
+  const database = new Database()
 
   async function handler (request, response) {
 
@@ -37,8 +40,6 @@ import { json } from './middlewares/json'
     return response.writeHead(404).end('Not Found')
 
 }
-
-const users = []
 
 http.createServer(handler)
 .listen(3333)
