2dfa04ab06d0407a0c39c8e04b814375b8e5638a
Jreng7 <jr.waydev@gmail.com>
2024-12-05 12:51:14 -0300

    commit day

--- rocketseat_node.js/src/server.js
+++ rocketseat_node.js/src/server.js
@@ -1,5 +1,9 @@
 import http from 'node:http'
 
+/**
+ *  CABEÇALHOS (TANTO DA REQUISIÇÃO QUANTO DA RESPOSTA) => SÃO METADADOS!
+ *  OS METADADOS SÃO INFORMAÇÕES PARA QUE CADA UM POSSA SABER LIDAR COM REQUISIÇÃO OU RESPOSTA.
+ */
 
 const users = []
 
@@ -7,6 +11,7 @@ const server = http.createServer((req, res) => {
   
   const { method, url } = req
 
+  // Criação de Usuários.
   if(method === 'POST' && url === '/users') {
 
     users.push({
@@ -15,14 +20,18 @@ const server = http.createServer((req, res) => {
       email: 'johndoe@gmail.com'
     })
 
-    return res.end('usuário criado com sucesso!')
+    return res.writeHead('201').end('Usuário Criado com Sucesso!')
   }
 
+  // Listagem de Usuários. 
   if(method === 'GET' && url === '/users') {
-    return res.end(users)
+    
+    return res
+    .setHeader('Content-type', 'application/json')  
+    .end(JSON.stringify(users))
   }
 
-    return res.end('Not Found')
+    return res.writeHead(404).end('Not Found')
 
 })
 
