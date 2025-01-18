bc8e4e1b4db20f9b6cea0b1e514a736fd5146b6f
Jreng7 <jr.waydev@gmail.com>
2025-01-17 22:52:42 -0300

    flag --watch no servidor.

--- node-rocketseat-rodrigo/src/server.js
+++ node-rocketseat-rodrigo/src/server.js
@@ -2,3 +2,8 @@
 import http from 'node:http' // Quando usamos "node:http" significa que estamos usando módulo nativo. Mas, por que usar?
 // Clareza: O prefixo node: deixa explícito que você está usando um módulo embutido no Node.js.
 
+const server = http.createServer((request, response) => {
+  return response.end("Servidor online com a flag --watch 2")
+})
+
+server.listen(3333)
