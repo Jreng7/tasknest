c97102ffd83c27d251e7a241f2728033d4cedabd
Jreng7 <jr.waydev@gmail.com>
2025-01-18 10:46:06 -0300

    estruturando dados

--- node-rocketseat-rodrigo/src/server.js
+++ node-rocketseat-rodrigo/src/server.js
@@ -2,6 +2,7 @@
 import http from 'node:http' // Quando usamos "node:http" significa que estamos usando módulo nativo. Mas, por que usar?
 // Clareza: O prefixo node: deixa explícito que você está usando um módulo embutido no Node.js.
 
+
 const server = http.createServer((request, response) => {
   const { method, url } = request
 
@@ -10,10 +11,18 @@ const server = http.createServer((request, response) => {
   }
 
   if(method === 'POST' && url === '/products'){
-    
+  
+    const buffers = [] // Banco de Dados.
+
+    for await (const chunk of request){
+      buffers.push(chunk)
+    }
+
+
+    return response.writeHead(201).end("Produto cadastrado com sucesso!")
   }
 
-  return response.writeHead(200).end("A URL é: " + url)
+  return response.writeHead(404).end("Not found - Rota não encontrada.")
 })
 
 server.listen(3333)
