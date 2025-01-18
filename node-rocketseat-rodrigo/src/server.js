e16e03558ae9336fb42e7727da020272d19b4308
Jreng7 <jr.waydev@gmail.com>
2025-01-17 21:36:47 -0300

    Por que usar? Clareza

--- node-rocketseat-rodrigo/src/server.js
+++ node-rocketseat-rodrigo/src/server.js
@@ -1,4 +1,4 @@
 
-import http from 'node:http' // Quando usamos "node:http" significa que estamos usando módulo nativo.
-
+import http from 'node:http' // Quando usamos "node:http" significa que estamos usando módulo nativo. Mas, por que usar?
+// Clareza: O prefixo node: deixa explícito que você está usando um módulo embutido no Node.js.
 
