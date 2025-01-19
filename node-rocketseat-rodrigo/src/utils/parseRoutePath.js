1fda2093c4b88e94eed726b7feb6043c23c9116b
Jreng7 <jr.waydev@gmail.com>
2025-01-18 22:22:55 -0300

    regex final

--- node-rocketseat-rodrigo/src/utils/parseRoutePath.js
+++ node-rocketseat-rodrigo/src/utils/parseRoutePath.js
@@ -1,4 +1,11 @@
 
 export function parseRoutePath(url){
-  const regexParams = /:([])/
+
+  const regexParams = /:([a-zA-Z]+)/g
+  const withParams = url.replaceAll(regexParams, "(?<$1>[a-z0-9-_]+)")
+  const params = new RegExp(withParams)
+
+
+  return params
 }
+
