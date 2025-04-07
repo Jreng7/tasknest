ef3feec37096ca7396706cd89517a01dad93ad0c
Jreng7 <jr.waydev@gmail.com>
2025-04-07 19:41:30 -0300

    feat(backend): create user schema with trimmed etc..

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, trim: true, unique: true },
+  email: { type: String, required: true, trim: true, unique: true },
+  password: { type: String, required: true, select: false }
+})
+
+export const User = mongoose.model('User', userSchema)
