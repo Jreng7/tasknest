ba45ab12c764bd1e43e1cce6f7ee50833dd08352
Jreng7 <jr.waydev@gmail.com>
2025-04-07 17:25:05 -0300

    chore(backend): chore: setup project structure with core middlewares and logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,22 @@
+// Core modules
 import express from 'express'
 
+// Config
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Middlewares de terceiros 
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+// Inicialização
 const app = express()
+
+// Middlewares globais
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+app.use(express.json())
 
 export default app;
