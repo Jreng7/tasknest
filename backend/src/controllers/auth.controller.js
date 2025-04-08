9365fcd65c3f1085d2d7afe1859187fd4bd42226
Jreng7 <jr.waydev@gmail.com>
2025-04-08 19:22:03 -0300

    feat(backend): install bcryptjs for password encryption

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,16 +1,35 @@
 import { User } from '../models/user.model.js'
+import 
 
 export const register = async (req, res) => {
 
   const { email, password, username } = req.body
 
   try {
+
+    // Verifica se já existe um usuário com o mesmo e-mail ou username
+    const existingUser = await User.findOne({ $or: [{ email}, {username}] })
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email or username already in use.'})
+    }
+
+    // Cria o novo usuário
     const newUser = new User({ username, email, password })
-    await newUser.save()
+
+    // Salva no banco
+    const userSaved = await newUser.save()
+
+    // Remove o password da resposta
+    const userResponse = {
+      _id: userSaved.id,
+      email: userSaved.email,
+      username: userSaved.username
+    }
+    res.status(201).json(userResponse)
   } catch (err) {
-    console.error(err)
+    console.error('🔴 Error registering user:', err)
+    return res.status(500).json({ message: 'Internal server error' })
   }
-  return res.status(201).json(newUser)
 }
 
 export const login = (req, res) => res.send('login')
