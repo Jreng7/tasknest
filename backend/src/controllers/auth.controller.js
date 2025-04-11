5c81a82b1bae10c39e6ff9e2d4ffcbd5c1099fb4
Jreng7 <jr.waydev@gmail.com>
2025-04-11 10:58:00 -0300

    refactor(backend): res.cookie e .json. Enviando diretamente

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,33 +3,29 @@ import { createAcessToken } from '../libs/jwt.js'
 import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
-
   const { email, password, username } = req.body
 
   try {
     const passwordHash = await bcrypt.hash(password, 10)
 
-    const existingUser = await User.findOne({ $or: [{ email}, {username}] })
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] })
     if (existingUser) {
-      return res.status(400).json({ message: 'Email or username already in use.'})
+      return res
+        .status(400)
+        .json({ message: 'Email or username already in use.' })
     }
 
     const newUser = new User({ username, email, password: passwordHash })
-
     const userSaved = await newUser.save()
-    const token = await createAcessToken({id: userSaved._id})
-    res.cookie('token', token).json({ message: "User created sucessfully" })
+    const token = await createAcessToken({ id: userSaved._id })
 
-    // Remove o password da resposta
-    const userResponse = {
+    res.cookie('token', token).json({
       _id: userSaved.id,
       email: userSaved.email,
       username: userSaved.username,
       createdAt: userSaved.createdAt,
-      updatedAt: userSaved.updatedAt
-    }
-    // Responde com usuário e o Token para o frontend capturar
-    res.status(201).json({user: userResponse, token})
+      updatedAt: userSaved.updatedAt,
+    })
   } catch (err) {
     console.error('🔴 Error registering user:', err)
     return res.status(500).json({ message: 'Internal server error' })
