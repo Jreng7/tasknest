72842c16f8bc868c695f1df9b1dcdae965516631
Jreng7 <jr.waydev@gmail.com>
2025-04-11 13:25:26 -0300

    refactor(backend): res.cookie colocando medidas de segurança

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -33,28 +33,26 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-  const { email, password, username } = req.body
+
+  const { email, password } = req.body
 
   try {
-    const passwordHash = await bcrypt.hash(password, 10)
+    const userFound = await User.findOne({ email })
+    if (!userFound) return res.status(400).json({ message: 'Email or username not found.' });
 
-    const existingUser = await User.findOne({ $or: [{ email }, { username }] })
-    if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: 'Email or username already in use.' })
-    }
+    const isMatch = await bcrypt.compare(password, userFound.password)
+    if(!isMatch) return res.status(400).json({ message: 'Incorrect password.' })
 
-    const newUser = new User({ username, email, password: passwordHash })
-    const userSaved = await newUser.save()
-    const token = await createAcessToken({ id: userSaved._id })
+    const token = await createAcessToken({ id: userFound._id })
 
-    res.cookie('token', token).json({
-      _id: userSaved.id,
-      email: userSaved.email,
-      username: userSaved.username,
-      createdAt: userSaved.createdAt,
-      updatedAt: userSaved.updatedAt,
+    res.cookie('token', token, {
+      httpOnly: true, // Bloqueia acesso via JS (previne XSS)
+    }).json({
+      _id: userFound.id,
+      email: userFound.email,
+      username: userFound.username,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
     })
   } catch (err) {
     console.error('🔴 Error registering user:', err)
