94476eecdcce9761313bab2371051dd66a116cdb
Jreng7 <jr.waydev@gmail.com>
2025-04-10 21:03:28 -0300

    reafacor(backend): new Promisse jwt.js

--- backend/src/libs/jwt.js
+++ backend/src/libs/jwt.js
@@ -2,8 +2,10 @@ import jwt from 'jsonwebtoken'
 import { secret } from '../config/config.js'
 
 export function createAcessToken(payload) {
-  // Gera um token usando id no payloud, secret e expiresIn para 1 dia.
-  jwt.sign(payload, secret.key, { expiresIn: '1d'}, (err) => {
-    if (err) console.error(err); // Finaliza a sentença; se não houver erro, pula para a próxima linha.
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, secret.key, { expiresIn: '1d'}, (err, token) => {
+      if (err) reject(err);
+      resolve(token)
+    })
   })
 }
