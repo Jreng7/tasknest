309c9746c50843bdb4f80da7a2b3037e923466e6
Jreng7 <jr.waydev@gmail.com>
2025-04-11 11:01:04 -0300

    feat(backend): criando controller de login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,4 +32,32 @@ export const register = async (req, res) => {
   }
 }
 
-export const login = (req, res) => res.send('login')
+export const login = async (req, res) => {
+  const { email, password, username } = req.body
+
+  try {
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] })
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: 'Email or username already in use.' })
+    }
+
+    const newUser = new User({ username, email, password: passwordHash })
+    const userSaved = await newUser.save()
+    const token = await createAcessToken({ id: userSaved._id })
+
+    res.cookie('token', token).json({
+      _id: userSaved.id,
+      email: userSaved.email,
+      username: userSaved.username,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    })
+  } catch (err) {
+    console.error('🔴 Error registering user:', err)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
